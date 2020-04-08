730e8b0439954546fd0453ab983e2072452f5f3f
Manuel de Jong <manuel@jaarsma-deboer.nl>
2020-04-08 21:37:45 +0200

    Initial commit

---
+++ reddit.py
@@ -0,0 +1,12 @@
+import praw
+import reddit_login
+
+reddit = praw.Reddit(
+    user_agent='windows:analyzer:v0.1 (by u/KillBottt)',
+    client_id=reddit_login.client_id,
+    client_secret=reddit_login.client_secret,
+    username=reddit_login.username,
+    password=reddit_login.password)
+
+print(reddit.user.me())
+print(reddit.read_only)
