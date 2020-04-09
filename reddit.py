b9546c5f82b6cba9842a149771897346dcd4b77e
Manuel de Jong <manuel@jaarsma-deboer.nl>
2020-04-09 13:17:51 +0200

    first setup for video and reddit api

--- reddit.py
+++ reddit.py
@@ -1,4 +1,5 @@
 import praw
+from praw.models import MoreComments
 import reddit_login
 
 reddit = praw.Reddit(
@@ -10,3 +11,20 @@ reddit = praw.Reddit(
 
 print(reddit.user.me())
 print(reddit.read_only)
+
+for submission in reddit.subreddit('askreddit').top('day', limit=1):
+    title = submission.title
+    comments = list(submission.comments)
+
+
+file = open("comments.txt", "w+")
+for i in range(10):
+    comment = comments[i]
+
+    if isinstance(comment, MoreComments):
+        continue
+
+    file.write('--------------------\n')
+    file.write(comment.body + '\n')
+
+file.close()
